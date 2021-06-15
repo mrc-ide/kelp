5160513dfe67e397101af8413695edd06810fdfe
Robert Ashton <r.ashton@imperial.ac.uk>
2021-06-15 17:53:53 +0100

    Update to testthat 3e

--- tests/testthat/test-util.R
+++ tests/testthat/test-util.R
@@ -1,5 +1,3 @@
-context("util")
-
 test_that("null-or-value works", {
   expect_equal(1 %||% NULL, 1)
   expect_equal(1 %||% 2, 1)
@@ -7,18 +5,17 @@ test_that("null-or-value works", {
   expect_equal(NULL %||% 2, 2)
 })
 
-test_that("binrary serialisation is transitive", {
-  f <- function(x, identical = TRUE) {
+test_that("binary serialisation is transitive", {
+  f <- function(x, ignore_function_env = FALSE) {
     y <- bin_to_object(object_to_bin(x))
-    expect_equal(x, y, check.attributes = FALSE)
+    expect_equal(x, y, ignore_attr = FALSE,
+                 ignore_function_env = ignore_function_env)
   }
   f(NULL)
   f(1)
-  f(f, identical = FALSE)
+  f(f, ignore_function_env = TRUE)
   f(1:10)
 
-  ## In contrast with string serialization above, binary serialization
-  ## is exact (as well as being about 10x faster which is nice).
   set.seed(1)
   x <- runif(100)
   f(x)
